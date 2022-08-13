02fe8a0ff73a7ba13d1fe5f2f8ae36cf60941259
dayun <81161750+dy6578ekdbs@users.noreply.github.com>
2022-08-14 00:43:49 +0900

    feat : 폰트 컴포넌트 생성

---
+++ src/components/Text.js
@@ -0,0 +1,30 @@
+import styled from "styled-components";
+
+export const PyeongChang = styled.p`
+  font-family: "PyeongChang";
+  font-weight: ${({ weight }) => weight};
+  font-size: ${({ size }) => size};
+  color: ${({ color }) => color};
+`;
+
+export const PyeongChang_Peace = styled.p`
+  font-family: "PyeongChangPeace";
+  font-weight: ${({ weight }) => weight};
+  font-size: ${({ size }) => size};
+  color: ${({ color }) => color};
+`;
+
+export const NanumSquare = styled.p`
+  font-family: "NanumSquare";
+  font-weight: ${({ weight }) => weight};
+  font-size: ${({ size }) => size};
+  color: ${({ color }) => color};
+`;
+
+export const Pretendard = styled.p`
+  font-family: "Pretendard-Regular";
+  font-weight: ${({ weight }) => weight};
+  font-size: ${({ size }) => size};
+  color: ${({ color }) => color};
+  line-height: ${({ height }) => height};
+`;
