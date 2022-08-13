9237a11cfd01c0f7b70dc4b84704993da6e853cc
dayun <81161750+dy6578ekdbs@users.noreply.github.com>
2022-08-14 00:44:30 +0900

    feat : 메인페이지 별 제목

---
+++ src/components/MainPage/StarTitle.js
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+import { PyeongChang } from "../../components/Text";
+
+import star3 from "../../images/stars/star3.svg";
+
+const StarTitle = ({ title }) => {
+  return (
+    <SmallTitle>
+      <TitleStar src={star3} />
+      <PyeongChang weight="700" color="#858585" size="15px">
+        {title}
+      </PyeongChang>
+      <TitleStar src={star3} />
+    </SmallTitle>
+  );
+};
+
+const SmallTitle = styled.div`
+  display: flex;
+`;
+
+const TitleStar = styled.img`
+  margin: 0 8px 0 8px;
+`;
+
+export default StarTitle;
