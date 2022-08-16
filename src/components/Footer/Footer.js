f6267ff15d12ae39acaddfc57ca6426df0b596ff
dayun <81161750+dy6578ekdbs@users.noreply.github.com>
2022-08-16 20:43:54 +0900

    feat : Footer 수정

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,12 +1,14 @@
 import styled from "styled-components";
 import github from "../../images/footer/github.svg";
 import insta from "../../images/footer/insta.svg";
+import graylikelion from "../../images/footer/graylikelion.svg";
+
 import { Pretendard } from "../Text";
 
 const Footer = () => {
   return (
     <Wrapper>
-      <hr style={{ marginBottom: "36px" }} />
+      <hr style={{ marginBottom: "36px", backgroundColor: "#A5A5A5" }} />
       <Pretendard color="#6C6C6C" weight="500" size="11px" height="18px">
         멋쟁이 사자처럼 10기 | https://open.kakao.com/o/sUzTxYue
       </Pretendard>
@@ -22,9 +24,20 @@ const Footer = () => {
           margin: "24px auto 0 auto",
         }}
       >
+        <img src={graylikelion} />
         <img src={github} />
         <img src={insta} />
       </div>
+
+      <Pretendard
+        color="rgba(165, 165, 165, 1)"
+        weight="300"
+        size="8px"
+        height="18px"
+        margin="22px auto 26px auto"
+      >
+        Copyright ⓒ Likelion Ewha 10th. All Rights Reserved.
+      </Pretendard>
     </Wrapper>
   );
 };
@@ -33,5 +46,6 @@ export default Footer;
 
 const Wrapper = styled.div`
   width: 100%;
+  height: 180px;
   text-align: center;
 `;
