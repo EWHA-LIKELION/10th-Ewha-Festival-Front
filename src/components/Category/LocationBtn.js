4a7278dd362115cdced9f02bc6764840e09e39d1
dayun <81161750+dy6578ekdbs@users.noreply.github.com>
2022-08-17 23:36:55 +0900

    feat : 건물 위치 버튼

---
+++ src/components/Category/LocationBtn.js
@@ -0,0 +1,33 @@
+import styled, { css } from "styled-components";
+
+const LocationBtn = ({ selected, children }) => {
+  return <Button selected={selected}>{children}</Button>;
+};
+
+export default LocationBtn;
+const Button = styled.button`
+  width: 58px;
+  height: 25px;
+
+  border-radius: 100px;
+  margin: 0 6px 0 6px;
+  text-align: center;
+  align-items: center;
+
+  font-family: "Pretendard";
+  font-weight: 400;
+  font-size: 14px;
+
+  ${props =>
+    props.selected
+      ? css`
+          background-color: var(--green3);
+          color: white;
+          border: none;
+        `
+      : css`
+          background-color: white;
+          border: 1px solid var(--green3);
+          color: var(--green3);
+        `}
+`;
