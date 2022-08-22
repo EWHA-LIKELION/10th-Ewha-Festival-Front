333b5c7609c0d6399fdf0602120d4021589e8683
dayun <81161750+dy6578ekdbs@users.noreply.github.com>
2022-08-22 23:22:37 +0900

    feat : booth 관련 api

--- src/api/services/boothservice.js
+++ src/api/services/boothservice.js
@@ -1,8 +1,10 @@
-import http from "../http";
+import { http } from "../http";
 
 const BoothService = {
-  getAllBooth: () => http.get("~/booth"),
-  getBooth: boothId => http.get("~/booth/boothId"),
+  getAllBooth: () => http.get(""), // 모든 부스 조회
+  getBooth: boothId => http.get(""), // 부스 상세
+  getKeywordBooth: (day, college) =>
+    http.get(`/booths?day=${day}&college=${college}`), // 요일, 장소에 따른 부스 조회
 };
 
 export default BoothService;
