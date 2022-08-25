7b35a09f6c2624ef31dddcfd4a2073f75ff92464
deliminzoo <minjoo9903@naver.com>
2022-08-26 00:19:21 +0900

    feat:마이페이지

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MainPage from "./pages/mainpage/MainPage";
 // 부스
 import Category from "./pages/boothpage/Category";
+// 마이페이지
+import MyUser from "./pages/mypage/MyUser";
+import MyManager from "./pages/mypage/MyManager";
+
 function App() {
   return (
     <BrowserRouter>
@@ -12,6 +16,9 @@ function App() {
         <Route exact path="/" element={<MainPage />} />
         {/* 부스 */}
         <Route exact path="/category" element={<Category />} />
+        {/* 마이페이지 */}
+        <Route exact path="/mypage_user" element={<MyUser />} />
+        <Route exact path="/mypage_manager" element={<MyManager />} />
       </Routes>
     </BrowserRouter>
   );
