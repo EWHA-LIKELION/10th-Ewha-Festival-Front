0f3d1ed9ee649087b4bf816f5a673438b4ff77b5
dayun <81161750+dy6578ekdbs@users.noreply.github.com>
2022-08-28 19:46:52 +0900

    feat : 좋아요 api 구현

--- src/api/services/boothservice.js
+++ src/api/services/boothservice.js
@@ -4,7 +4,12 @@ const BoothService = {
   // 요일, 장소에 따른 부스 조회
   getKeywordBooth: (day, college) =>
     http.get(`/booths?day=${day}&college=${college}`),
-
+  likeBooth: boothId =>
+    http.post(`/booths/${boothId}/likes/
+`),
+  unLikeBooth: boothId =>
+    http.delete(`/booths/${boothId}/likes/
+`),
   // 이 밑으로 아직 작업 안함
   getAllBooth: () => http.get(""), // 모든 부스 조회
   getBooth: boothId => http.get(""), // 부스 상세
