7a2d0b8e7d51cca23a11926862a97c84233fb585
이채원 <chaewon@ichaewon-ui-MacBookPro.local>
2022-08-29 01:59:24 +0900

    feat: 로그인, 회원가입 api 추가

--- src/api/services/userservice.js
+++ src/api/services/userservice.js
@@ -1,7 +1,23 @@
-import http from "../http";
+import http from "../http.js";
 
 const UserService = {
   getUser: user => http.get("~/user"),
+
+  //로그인 
+  getUser: (id, password) =>
+    http.post("/accounts/login/",{
+      username: id,
+      password: password,
+    }),
+  
+  //회원가입 api
+  postUser: (id,password,name) =>
+    http.post("/accounts/signup/",{
+    username: id,
+    password: password,
+    nickname: name,
+  }),
 };
+
 
 export default UserService;
