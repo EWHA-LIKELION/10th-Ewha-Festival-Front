f856f68a4e1fb3841bd95234b98580f5454e5af6
dayun <81161750+dy6578ekdbs@users.noreply.github.com>
2022-08-28 17:52:03 +0900

    feat : http에 token 추가

--- src/api/http.js
+++ src/api/http.js
@@ -1,3 +1,14 @@
 import axios from "axios";
 
-export const http = axios.create({ baseURL: "http://localhost:3000/" });
+export const http = axios.create({
+  baseURL: "http://43.200.53.202",
+});
+
+http.defaults.withCredentials = true;
+
+const token = JSON.parse(localStorage.getItem("token"));
+console.log("토큰 : ", token);
+
+http.defaults.headers.common["Authorization"] = token
+  ? `Bearer ${token}`
+  : null;
