429f4401563be5643bcfd56f8ed8ab57321e329e
이채원 <chaewon@ichaewon-ui-MacBookPro.local>
2022-08-30 22:17:17 +0900

    feat: 로그인 회원가입 api 추가

--- src/api/user.js
+++ src/api/user.js
@@ -7,11 +7,21 @@ export const GetUser = async (id, password) => {
   
       return Promise.resolve(response.data);
     } catch (error) {
-      return Promise.reject(error, "로그인 실패");
+      return Promise.reject(error.response.data, "로그인 실패");
     }
   };
 
-  // 회원가입
+// 프로필 조회 
+export const GetProfile = async () => {
+  try {
+    const response = await UserService.getProfile();
+    return Promise.resolve(response.data);
+  } catch (error) {
+    return Promise.reject(error, "프로필 조회 실패");
+  }
+};
+
+// 회원가입
 export const PostUser = async (id, password, name) => {
     try {
       const response = await UserService.postUser(id, password, name);
