f5d2999eba94cd19b6482d04e72a2f675561ec2d
이채원 <chaewon@ichaewon-ui-MacBookPro.local>
2022-08-30 22:16:05 +0900

    feat: 로그인 회원가입 api 추가

--- src/api/services/userservice.js
+++ src/api/services/userservice.js
@@ -1,18 +1,23 @@
-import http from "../http.js";
+import {http} from "../http.js";
+import axios from "axios";
+
 
 const UserService = {
-  getUser: user => http.get("~/user"),
+  //프로필 조회
+  getProfile: () => http.get("/accounts/"),
 
   //로그인 
   getUser: (id, password) =>
-    http.post("/accounts/login/",{
+      //http.post("/accounts/login/"){
+      axios.post("http://43.200.53.202/accounts/login/",{
       username: id,
       password: password,
     }),
   
   //회원가입 api
   postUser: (id,password,name) =>
-    http.post("/accounts/signup/",{
+    //http.post("/accounts/signup/"){
+    axios.post("http://43.200.53.202/accounts/signup/",{
     username: id,
     password: password,
     nickname: name,
