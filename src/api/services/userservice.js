a422232ef4d2067021aaf9bdd278a7fd891315a0
dayun <81161750+dy6578ekdbs@users.noreply.github.com>
2022-08-31 17:22:22 +0900

    feat :프로필 조회 함수 변경

--- src/api/services/userservice.js
+++ src/api/services/userservice.js
@@ -1,28 +1,31 @@
-import {http} from "../http.js";
+import { http } from "../http.js";
 import axios from "axios";
-
 
 const UserService = {
   //프로필 조회
-  getProfile: () => http.get("/accounts/"),
+  getProfile: token =>
+    axios.get("https://api.rewha2022.com/accounts/", {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    }),
 
-  //로그인 
+  //로그인
   getUser: (id, password) =>
-      //http.post("/accounts/login/"){
-      axios.post("http://43.200.53.202/accounts/login/",{
+    //http.post("/accounts/login/"){
+    axios.post("http://43.200.53.202/accounts/login/", {
       username: id,
       password: password,
     }),
-  
-  //회원가입 api
-  postUser: (id,password,name) =>
-    //http.post("/accounts/signup/"){
-    axios.post("http://43.200.53.202/accounts/signup/",{
-    username: id,
-    password: password,
-    nickname: name,
-  }),
-};
 
+  //회원가입 api
+  postUser: (id, password, name) =>
+    //http.post("/accounts/signup/"){
+    axios.post("http://43.200.53.202/accounts/signup/", {
+      username: id,
+      password: password,
+      nickname: name,
+    }),
+};
 
 export default UserService;
