3d1471d5583fcf469e75044f991f8e6e3786e7fb
이채원 <chaewon@ichaewon-ui-MacBookPro.local>
2022-08-31 17:56:55 +0900

    feat: base URL 변경

--- src/api/http.js
+++ src/api/http.js
@@ -1,14 +1,14 @@
+
 import axios from "axios";
 
 export const http = axios.create({
-  baseURL: "http://43.200.53.202",
+  baseURL: "https://api.rewha2022.com",
 });
 
 http.defaults.withCredentials = true;
 
 const token = JSON.parse(localStorage.getItem("token")) ?? false;
 console.log("토큰 : ", token);
-
 
 http.defaults.headers.common["Authorization"] = token
   ? `Bearer ${token}`
