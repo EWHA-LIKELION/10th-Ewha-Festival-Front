4dd45dec3cc992b1ad6978355299009f16422571
이채원 <chaewon@ichaewon-ui-MacBookPro.local>
2022-09-08 14:37:33 +0900

    chore: 리액트 아이콘 변경

--- src/pages/authpage/RegisterPage.js
+++ src/pages/authpage/RegisterPage.js
@@ -8,10 +8,10 @@ import axios from "axios";
 //images
 import title from "../../images/main/title.svg";
 import { FiUser } from "react-icons/fi";
-import lockIcon from "../../images/signup/lockIcon.svg";
+import { MdOutlineLockOpen } from "react-icons/md";
 import { IoMdCheckmarkCircle } from "react-icons/io";
-import flowerIcon from "../../images/signup/flowerIcon.svg";
-import keyIcon from "../../images/signup/keyIcon.svg";
+import { BsFlower2 } from "react-icons/bs";
+import {MdOutlineVpnKey} from "react-icons/md";
 import { AiFillInfoCircle } from "react-icons/ai";
 // 모달창
 import RegisterModal from "../../components/Register/RegisterModal";
@@ -113,9 +113,7 @@ const RegisterPage = () => {
               />
             </IdWrapper>
             <PwWrapper>
-              <object type="image/svg+xml" data={lockIcon} className="lockIcon">
-                <img src={lockIcon} />
-              </object>
+              <MdOutlineLockOpen className="lockIcon"/>
               <input
                 value={password}
                 className="PW"
@@ -126,9 +124,7 @@ const RegisterPage = () => {
               />
             </PwWrapper>
             <PwWrapper>
-              <object type="image/svg+xml" data={lockIcon} className="lockIcon">
-                <img src={lockIcon} />
-              </object>
+                <MdOutlineLockOpen className="lockIcon"/>
               <input
                 value={password2}
                 className="PW2"
@@ -146,9 +142,7 @@ const RegisterPage = () => {
               />
             </PwWrapper>
             <NameWrapper>
-              <object type="image/svg+xml" data={flowerIcon} className="flowerIcon">
-                <img src={flowerIcon} />
-              </object>
+              <BsFlower2 className="flowerIcon"/>
               <input
                 value={name}
                 placeholder="닉네임"
@@ -156,9 +150,7 @@ const RegisterPage = () => {
               />
             </NameWrapper>
             <SecreteWrapper>
-              <object type="image/svg+xml" data={keyIcon} className="keyIcon">
-                <img src={keyIcon} />
-              </object>
+              <MdOutlineVpnKey className="keyIcon"/>
               <input
                 value={secreteWord}
                 placeholder="비밀단어"
@@ -227,7 +219,8 @@ const IdWrapper = styled.div`
     width: 294px;
   }
   .idIcon {
-    width: 22px;
+    width: 18px;
+    height: 18px;
     position: absolute;
     z-index: 2;
     margin: 11px 0 0 12px;
@@ -240,6 +233,7 @@ const PwWrapper = styled.div`
   }
   .lockIcon{
     width: 18px;
+    height: 18px;
     position: absolute;
     z-index: 2;
     margin: 27px 0 0 12px;
@@ -259,6 +253,8 @@ const NameWrapper = styled.div`
   }
   .flowerIcon{
     width: 18px;
+    height: 18px;
+    color: #797878;
     position: absolute;
     z-index: 2;
     margin: 11px 0 0 12px;
@@ -272,6 +268,8 @@ const SecreteWrapper = styled.div`
   }
   .keyIcon{
     width: 18px;
+    height: 18px;
+    color: #797878;
     position: absolute;
     z-index: 2;
     margin: 11px 0 0 12px;
