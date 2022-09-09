da84354ef22db477315d53c0e91d138a85fffb86
dy6578ekdbs <dy6578ekdbs@gmail.com>
2022-09-09 11:57:03 +0900

    style : 지도 이미지 추가

--- src/_mock/boothMap.js
+++ src/_mock/boothMap.js
@@ -1,4 +1,23 @@
 import map from "../images/map.svg";
 import heart from "../images/heart.svg";
-export const boothMaps = [null, map, heart, map, heart, map, heart, map, heart];
-// 임시
+
+import frontDoor from "../images/category/frontDoor.svg";
+import edu from "../images/category/edu.svg";
+import auditorium from "../images/category/auditorium.svg";
+import hooyou from "../images/category/hooyou.svg";
+import posco from "../images/category/posco.svg";
+import student from "../images/category/student.svg";
+import life from "../images/category/life.svg";
+import sin from "../images/category/sin.svg";
+
+export const boothMaps = [
+  null,
+  frontDoor,
+  edu,
+  auditorium,
+  hooyou,
+  posco,
+  student,
+  life,
+  sin,
+];
