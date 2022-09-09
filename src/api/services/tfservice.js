4ac23e902dd497cd4823655457152d9cc2365fe1
dy6578ekdbs <dy6578ekdbs@gmail.com>
2022-09-09 12:33:09 +0900

    test : 배포테스트, api 수정

--- src/api/services/tfservice.js
+++ src/api/services/tfservice.js
@@ -2,19 +2,20 @@ import { http } from "../http";
 
 const TfService = {
   getAllNotice: () => http.get("/notices/"), // 모든 공지 조회
-  getNotice: (noticeId) => http.get(`/notices/${noticeId}/`), // 공지 상세
-  deleteNotice: (noticeId) => http.delete(`/notices/${noticeId}/`), //댓글 삭제
+  getNotice: noticeId => http.get(`/notices/${noticeId}/`), // 공지 상세
+  deleteNotice: noticeId => http.delete(`/notices/${noticeId}/`), //댓글 삭제
   // 공지사항 수정
-  putNotice: (noticeId, title, content) => http.patch(`/notices/${noticeId}`, {
-    noticeId: noticeId,
-    title: title,
-    content: content,
-  }), 
+  putNotice: (noticeId, title, content) =>
+    http.patch(`/notices/1`, {
+      title: title,
+      content: content,
+    }),
   // 공지사항 작성
-  postNotice: (title, content) => http.post('/notices/', { 
-    title:title,
-    content:content
-  }),
+  postNotice: (title, content) =>
+    http.post("/notices/", {
+      title: title,
+      content: content,
+    }),
 };
 
 export default TfService;
